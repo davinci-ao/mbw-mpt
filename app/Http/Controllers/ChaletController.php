a1bb5468b7c7ff31cb457b85ec9b6a9321d3651e
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-09-10 13:22:42 +0200

    added Chalet model, controller, factory and migration

---
+++ app/Http/Controllers/ChaletController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chalet;
+use Illuminate\Http\Request;
+
+class ChaletController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Chalet  $chalet
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Chalet $chalet)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Chalet  $chalet
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chalet $chalet)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Chalet  $chalet
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chalet $chalet)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Chalet  $chalet
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chalet $chalet)
+    {
+        //
+    }
+}
