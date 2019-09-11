2a53909389b9528270b1aeb004203a35d11b9ce6
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-09-11 12:19:37 +0200

    added index, and chalet migration

---
+++ database/factories/ChaletFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Chalet;
+use Faker\Generator as Faker;
+
+$factory->define(Chalet::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
