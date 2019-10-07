7cc9aa5db2372d398d0bd3c38dcd167b59ffe053
Jochem de Kruijf <jochemdekruijfff@outlook.com>
2019-10-07 15:01:54 +0200

    added Calendar to chalet page

--- public/js/app.js
+++ public/js/app.js
@@ -115,7 +115,6 @@ new Vue({
     };
   }
 });
-
 new Vue({
   el: '#footer',
   vuetify: new Vuetify(),
@@ -124,6 +123,228 @@ new Vue({
       links: ['Home', 'Chalets', 'Contact'],
       icons: ['fab fa-facebook', 'fab fa-twitter', 'fab fa-google-plus', 'fab fa-linkedin', 'fab fa-instagram']
     };
+  }
+});
+new Vue({
+  el: '#calendar',
+  vuetify: new Vuetify(),
+  data: function data() {
+    return {
+      lang: 'nl',
+      today: 'locale',
+      focus: 'locale',
+      type: 'month',
+      typeToLabel: {
+        month: 'Maand',
+        week: 'Week',
+        day: 'Dag'
+      },
+      start: null,
+      end: null,
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+      events: [{
+        name: 'Vacation',
+        details: 'Going to the beach!',
+        start: '2018-12-29',
+        end: '2019-01-01',
+        color: 'blue'
+      }, {
+        name: 'Meeting',
+        details: 'Spending time on how we do not have enough time',
+        start: '2019-01-07 09:00',
+        end: '2019-01-07 09:30',
+        color: 'indigo'
+      }, {
+        name: 'Large Event',
+        details: 'This starts in the middle of an event and spans over multiple events',
+        start: '2018-12-31',
+        end: '2019-01-04',
+        color: 'deep-purple'
+      }, {
+        name: '3rd to 7th',
+        details: 'Testing',
+        start: '2019-01-03',
+        end: '2019-01-07',
+        color: 'cyan'
+      }, {
+        name: 'Big Meeting',
+        details: 'A very important meeting about nothing',
+        start: '2019-01-07 08:00',
+        end: '2019-01-07 11:30',
+        color: 'red'
+      }, {
+        name: 'Another Meeting',
+        details: 'Another important meeting about nothing',
+        start: '2019-01-07 10:00',
+        end: '2019-01-07 13:30',
+        color: 'brown'
+      }, {
+        name: '7th to 8th',
+        start: '2019-01-07',
+        end: '2019-01-08',
+        color: 'blue'
+      }, {
+        name: 'Lunch',
+        details: 'Time to feed',
+        start: '2019-01-07 12:00',
+        end: '2019-01-07 15:00',
+        color: 'deep-orange'
+      }, {
+        name: '30th Birthday',
+        details: 'Celebrate responsibly',
+        start: '2019-01-03',
+        color: 'teal'
+      }, {
+        name: 'New Year',
+        details: 'Eat chocolate until you pass out',
+        start: '2019-01-01',
+        end: '2019-01-02',
+        color: 'green'
+      }, {
+        name: 'Conference',
+        details: 'The best time of my life',
+        start: '2019-01-21',
+        end: '2019-01-28',
+        color: 'grey darken-1'
+      }, {
+        name: 'Hackathon',
+        details: 'Code like there is no tommorrow',
+        start: '2019-01-30 23:00',
+        end: '2019-02-01 08:00',
+        color: 'black'
+      }, {
+        name: 'event 1',
+        start: '2019-01-14 18:00',
+        end: '2019-01-14 19:00',
+        color: '#4285F4'
+      }, {
+        name: 'event 2',
+        start: '2019-01-14 18:00',
+        end: '2019-01-14 19:00',
+        color: '#4285F4'
+      }, {
+        name: 'event 5',
+        start: '2019-01-14 18:00',
+        end: '2019-01-14 19:00',
+        color: '#4285F4'
+      }, {
+        name: 'event 3',
+        start: '2019-01-14 18:30',
+        end: '2019-01-14 20:30',
+        color: '#4285F4'
+      }, {
+        name: 'event 4',
+        start: '2019-01-14 19:00',
+        end: '2019-01-14 20:00',
+        color: '#4285F4'
+      }, {
+        name: 'event 6',
+        start: '2019-01-14 21:00',
+        end: '2019-01-14 23:00',
+        color: '#4285F4'
+      }, {
+        name: 'event 7',
+        start: '2019-01-14 22:00',
+        end: '2019-01-14 23:00',
+        color: '#4285F4'
+      }]
+    };
+  },
+  computed: {
+    title: function title() {
+      var start = this.start,
+          end = this.end;
+
+      if (!start || !end) {
+        return '';
+      }
+
+      var startMonth = this.monthFormatter(start);
+      var endMonth = this.monthFormatter(end);
+      var suffixMonth = startMonth === endMonth ? '' : endMonth;
+      var startYear = start.year;
+      var endYear = end.year;
+      var suffixYear = startYear === endYear ? '' : endYear;
+      var startDay = start.day + this.nth(start.day);
+      var endDay = end.day + this.nth(end.day);
+
+      switch (this.type) {
+        case 'month':
+          return "".concat(startMonth, " ").concat(startYear);
+
+        case 'week':
+        case '4day':
+          return "".concat(startMonth, " ").concat(startDay, " ").concat(startYear, " - ").concat(suffixMonth, " ").concat(endDay, " ").concat(suffixYear);
+
+        case 'day':
+          return "".concat(startMonth, " ").concat(startDay, " ").concat(startYear);
+      }
+
+      return '';
+    },
+    monthFormatter: function monthFormatter() {
+      return this.$refs.calendar.getFormatter({
+        timeZone: 'GMT',
+        month: 'long'
+      });
+    }
+  },
+  mounted: function mounted() {
+    this.$refs.calendar.checkChange();
+  },
+  methods: {
+    viewDay: function viewDay(_ref) {
+      var date = _ref.date;
+      this.focus = date;
+      this.type = 'day';
+    },
+    getEventColor: function getEventColor(event) {
+      return event.color;
+    },
+    setToday: function setToday() {
+      this.focus = this.today;
+    },
+    prev: function prev() {
+      this.$refs.calendar.prev();
+    },
+    next: function next() {
+      this.$refs.calendar.next();
+    },
+    showEvent: function showEvent(_ref2) {
+      var _this = this;
+
+      var nativeEvent = _ref2.nativeEvent,
+          event = _ref2.event;
+
+      var open = function open() {
+        _this.selectedEvent = event;
+        _this.selectedElement = nativeEvent.target;
+        setTimeout(function () {
+          return _this.selectedOpen = true;
+        }, 10);
+      };
+
+      if (this.selectedOpen) {
+        this.selectedOpen = false;
+        setTimeout(open, 10);
+      } else {
+        open();
+      }
+
+      nativeEvent.stopPropagation();
+    },
+    updateRange: function updateRange(_ref3) {
+      var start = _ref3.start,
+          end = _ref3.end;
+      // You could load events from an outside source (like database) now that we have the start and end dates on the calendar
+      this.start = start;
+      this.end = end;
+    },
+    nth: function nth(d) {
+      return d > 3 && d < 21 ? 'th' : ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'][d % 10];
+    }
   }
 });
 
