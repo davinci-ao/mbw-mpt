e41e0ce6ae5ef666b6ab666426d776e98d39ecf1
Danny <dannydejonge1710@outlook.com>
2019-10-07 22:29:34 +0200

    Create edit and delete of chalets only visible when logged in

--- resources/views/chalets/index.blade.php
+++ resources/views/chalets/index.blade.php
@@ -3,23 +3,37 @@
 @section('content')
 
 <h4>Data:</h4><br>
-<a class="btn btn-primary" href="{{ URL::to('chalets/create') }}">voeg Chalet toe</a>
-<hr>
+
+<!-- only show create button when logged in -->
+
+@if (Auth::check())
+  <a class="btn btn-primary" href="{{ URL::to('chalets/create') }}">voeg Chalet toe</a>
+  <hr>
+@endif
 
 @foreach ($chaletData as $chalet)
   <td>{{ $chalet->name }}</td>
   <td>{{ $chalet->description}}</td>
   <td>{{ $chalet->price}}</td>
-  <a href="{{ route('chalets.edit',$chalet->id)}}" class="btn btn-primary">Edit</a>
-    
 
-  <form action="{{ route('chalets.destroy',$chalet->id)}}" method="post">
-    @csrf
-    @method('DELETE') 
-    <button class="btn btn-danger" type="submit">Delete</button>
-  </form>
+  <!-- only show edit button when logged in -->
 
-  <hr>
+  @if (Auth::check())
+    <a href="{{ route('chalets.edit',$chalet->id)}}" class="btn btn-primary">Edit</a>
+  @endif  
+
+
+  <!-- only show delete button when logged in -->
+
+  @if (Auth::check())
+    <form action="{{ route('chalets.destroy',$chalet->id)}}" method="post">
+      @csrf
+      @method('DELETE') 
+      <button class="btn btn-danger" type="submit">Delete</button>
+    </form>
+    <hr>
+  @endif
+
 @endforeach
 
 @endsection
