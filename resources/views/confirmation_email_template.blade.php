69ce6bd6ef04df8440eeca761b94d426a2696429
dannydejonge1710 <31654836+dannydejonge1710@users.noreply.github.com>
2019-12-09 14:41:25 +0100

    removed s from chalets

--- resources/views/confirmation_email_template.blade.php
+++ resources/views/confirmation_email_template.blade.php
@@ -10,7 +10,7 @@
 <b>Aankomst</b> {{ Carbon\Carbon::parse($data['arrival'])->format('d-m-Y') }} om 11 uur <br>
 <b>Vetrek</b> {{ Carbon\Carbon::parse($data['departure'])->format('d-m-Y') }} om 12 uur  <br>
 <br>
-  Bedankt voor het boeken van {{$data['chalets']}}.<br>
+  Bedankt voor het boeken van {{$data['chalet']}}.<br>
   U heeft geresveerd voor {{$data['people']}} personen.<br>
   De totaalprijs bedraagt {{$data['price']}}.
 </p>
