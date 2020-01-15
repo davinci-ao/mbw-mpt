edcff810d86c9e192fb816c1b0ee356a039c7b74
Lars Gruis <larsgruis@gmail.com>
2020-01-15 09:59:17 +0100

    Changed holidayparks styling

--- resources/views/holidayparks/index.blade.php
+++ resources/views/holidayparks/index.blade.php
@@ -9,26 +9,32 @@
     <a href="{{ URL::to('holidayparks/create') }}" class="btn btn-primary chalet-add-btn" style="margin-bottom: 10px;">Voeg een vakantiepark toe</a>
 @endif
 
+<div class="card-deck">
+
 @foreach ($holidayparks as $holidaypark)
 
-<div class="card w-25 holidaypark-card">
-  <div class="card-body">
-    <h5 class="card-title holidaypark-title">{{ $holidaypark->holidaypark_name }}</h5> @if (Auth::check())<a style="  padding: 5px;" href="{{ route('holidayparks.edit',$holidaypark->id)}}" ><small><i class="fas fa-edit"></i></small></a>  @endif
-    
-    <p class="card-text holidaypark-description">{{ $holidaypark->description }}</p>
+  <div class="card holidaypark-cards no-flex">
+      <div class="card-body">
+        <h5 class="card-title">{{ $holidaypark->holidaypark_name }}</h5>
+        <p class="card-text">{{ $holidaypark->description }}</p>
+        <a href="{{ url('chalets?holidaypark=' . $holidaypark->id)}}" class="btn btn-primary holidaypark-btn">Bekijken</a>
 
-    <a href="{{ url('chalets?holidaypark=' . $holidaypark->id)}}" class="btn btn-primary holidaypark-btn">Bekijken</a>
-
-    @if (Auth::check())
-      <form action="{{ route('holidayparks.destroy',$holidaypark->id)}}" method="post">
-        @csrf
-        @method('DELETE') 
-        <button style=" margin: 5px; font-size: 20px; float: right;" onclick="return confirm('Weet je het zeker dat je dit vakantiepark wil verwijderen?');" type="submit"><i class="fas fa-trash-alt"></i></button>
-      </form>
-    @endif  
+        <div class="holidaypark-btns">
+            @if (Auth::check())<a class="edit-holidaypark-btn" href="{{ route('holidayparks.edit',$holidaypark->id)}}" ><small><i class="fas fa-edit"></i></small></a>  @endif
+          
+            @if (Auth::check())
+              <form class="inline-form" action="{{ route('holidayparks.destroy',$holidaypark->id)}}" method="post">
+                @csrf
+                @method('DELETE') 
+                <button onclick="return confirm('Weet je het zeker dat je dit vakantiepark wil verwijderen?');" type="submit"><i class="fas fa-trash-alt"></i></button>
+              </form>
+            @endif 
+          </div> 
+      </div>
   </div>
-</div>
 
 @endforeach 
+
+</div>
 
 @endsection 
